b43620790007f1a28dc0c81ef3502c02c08fe617
iremberber <iremberber0@gmail.com>
2023-10-18 22:31:19 +0300

    1-5 Day of Python
    Day 1- Print
    Day 2- Input and Veriables
    Day 3- Concatenation
    Day 4- Project Day
    Day 5- If Else

---
+++ replit-1-5DayOfPython.py
@@ -0,0 +1,105 @@
+#Day1
+print("Hello world!")
+
+
+print("""HELLO
+WORLD
+WHATs
+UP""")
+
+#task1
+
+print("""""İrem
+18 October 2023
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so ı can do this from my phone whereever I happen to be. No excuses for not coding from middle of a field!
+I'm feeling🤨
+You can follow my progress at replit.com/@IremBerber""""")
+
+#Day2
+
+MyName = input("Name: ")
+MyAge = input("Age: ")
+print("Nice to meet you!")
+print()
+print("So, you are")
+print(MyName)
+print("and you are")
+print(MyAge)
+print("years old.")
+
+#task2
+
+print("=== Your Song Generator ===")
+print("""You'll be asked a bunch of questions
+then we'll make you up an amazing
+song, totally copyright free 😭""")
+print()
+person = input("Name a person famous for something good: ")
+thing = input("Name a thing they did: ")
+place = input("Name a place you like: ")
+rhyme = input("Give me a verb that rhymes with your person's name: ")
+print()
+print("There was a person called", person)
+print("Who did something cool like", thing, "at the wonderful", place, "where you'll find me", rhyme)
+
+#Day3
+
+myName = input("What's your name? ")
+myLunch = input("What are you having for lunch? ")
+print(myName, "is going to be chowing down on", myLunch, "very soon!")
+
+#task3
+
+print("=== Your Song Generator ===")
+print("""You'll be asked a bunch of questions
+then we'll make you up an amazing
+song, totally copyright free 😭""")
+print()
+person = input("Name a person famous for something good: ")
+thing = input("Name a thing they did: ")
+place = input("Name a place you like: ")
+rhyme = input("Give me a verb that rhymes with your person's name: ")
+print()
+print("There was a person called", person)
+print("Who did something cool like", thing, "at the wonderful", place, "where you'll find me", rhyme)
+
+#Day4 Project Day
+
+print("YOUR ADVENTURE SIMULATOR")
+
+name = input("What is your name?:")
+enemy = input("What is your enemy's name?:")
+power = input("What is your super power?:")
+
+print()
+
+print("It was a cold winter day.", name, "were walking down the street when" , name," saw ", enemy ," standing in the middle of the road." , name , "was know, the" , enemy, "is planing the attack. So", name, "use" , name +"'s secret power;", power)
+
+#Day5
+
+myName = input("What's your name?: ")
+if myName == "İrem":
+    print("Welcome Girl!")
+    print("You are awesome!")
+else:
+    print("Who are you??")
+
+#task
+
+print("Which character are you?")
+
+q1 = input("What is your favorite color? ")
+if q1 == "red":
+  print("You are Celestia.")
+elif q1 == "blue":
+  print("You are Eve.")
+elif q1 == "green":
+  print("You are Gilgamesh.")
+elif q1 == "yellow":
+  print("You are Hermes.")
+elif q1 == "orange":
+  print("You are Isis.")
+else:
+  print("Have no idea.")
