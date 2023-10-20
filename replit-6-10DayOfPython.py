8972aff6b3afcc445c7d41b9fddaf97aecf322ed
iremberber <iremberber0@gmail.com>
2023-10-20 23:31:44 +0300

    Create replit-6-10DayOfPython.py

---
+++ replit-6-10DayOfPython.py
@@ -0,0 +1,15 @@
+#Day6
+
+print("SECURE LOGIN")
+username = input("Username > ")
+password = input("Password > ")
+
+if username == "mark" and password == "password":
+  print("Welcome Mark!")
+elif username == "irem" and password == "iremirem":
+  print("Welcome master!")
+else:
+  print("Go away!")
+
+#task6
+
