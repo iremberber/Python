3b19b4dcac778304bb3beaec7b4cd64598fc4351
iremberber <iremberber0@gmail.com>
2023-10-21 22:59:51 +0300

    Update replit-6-10DayOfPython.py

--- replit-6-10DayOfPython.py
+++ replit-6-10DayOfPython.py
@@ -11,5 +11,37 @@ elif username == "irem" and password == "iremirem":
 else:
   print("Go away!")
 
-#task6
+#day7
+
+order = input("What would you like to order: pizza or hamburger? ")
+if order == "hamburger":
+  print("Thank you.")
+  cheese = input("Do you want cheese?")
+  if cheese == "yes":
+    print("You got it.")
+  else: 
+    print("No cheese it is.")
+elif order == "pizza":
+  print("Pizza coming up.")
+  toppings = input("Do you want pepperoni on that?")
+  if toppings == "yes":
+    print("We will add pepperoni.")
+  else:
+    print("Your pizza will not have pepperoni.")
+
+#task7
+
+print("Fake Fan Finder")
+
+q1 = input("Are you a Swiftie?")
+
+if q1 == "yes":
+    q2 = input("What is your favorite song?")
+    if q2 == "IDK":
+        print("You are not a fan!")
+    else:
+        print("You are a fan!")
+else:
+  print("That's fine")
+
 
