37f3a928766edac8e93efcd80138fa6caaecf4e0
iremberber <iremberber0@gmail.com>
2023-10-25 22:35:43 +0300

    Update replit-11-15DayOfPython.py

--- replit-11-15DayOfPython.py
+++ replit-11-15DayOfPython.py
@@ -1,2 +1,10 @@
 #Day11
 
+print("How many seconds are in a year?")
+
+leap = input("Is this a leap year? Yes or No only.")
+
+if leap == "Yes":
+  print(60 * 60 * 24 * 365 * 366)
+else:
+  print(60 * 60 * 24 * 365 * 365)
