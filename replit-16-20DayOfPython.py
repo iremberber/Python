16215d20c54c16f700444d9f3c4ee98ff8d58060
iremberber <iremberber0@gmail.com>
2023-12-08 23:54:27 +0300

    Update replit-16-20DayOfPython.py

--- replit-16-20DayOfPython.py
+++ replit-16-20DayOfPython.py
@@ -17,3 +17,45 @@ while True:
 print("Thanks for playing!")
 print("You got the correct lyrics in", counter, "attempt(s).")
 
+#day17
+from getpass import getpass as input
+
+counter1 = 0
+counter2 = 0
+while True:
+
+  print("You should use 'r' for rock, 'p' for paper, and 's' for scissors. After your move, please press enter.")
+  player_1 = input("Player 1, what is your move? ")
+
+  player_2 = input("Player 2, what is your move? ")
+
+  if player_1=="p" and player_2=="s":
+    print("player 2 wins")
+    counter2 +=1
+
+  if player_1=="p" and player_2=="r":
+    print("player 1 wins")
+    counter1 +=1
+
+  if player_1=="s" and player_2=="r":
+    print("player 2 wins")
+    counter2 +=1
+
+  if player_1=="s" and player_2=="p":
+    print("player 1 wins")
+  counter1 +=1
+
+  if player_1=="r" and player_2=="s":
+    print("player 1 wins")
+    counter1 +=1
+
+  if player_1=="r" and player_2=="p":
+    print("player2 2 wins")
+    counter2 +=1
+
+  if player_1==player_2:
+    print("draw")
+
+  if counter1==3 or counter2==3:
+    print("game over")
+    break
