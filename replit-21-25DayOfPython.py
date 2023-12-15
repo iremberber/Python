cab4248f5221f99d0ad34ec1ce9c42160346c8db
iremberber <iremberber0@gmail.com>
2023-12-15 19:27:25 +0300

    Day 21
    Libraries

--- replit-21-25DayOfPython.py
+++ replit-21-25DayOfPython.py
@@ -13,3 +13,30 @@ for i in range(11):
 
 print("You got " + str(counter) + " out of 10 questions correct.")
 
+#Day22 Libraries
+
+import random
+
+print("Totally Random One-Million-to-One")
+
+correct_number = random.randint(1, 1000000)
+attempt = 1 
+
+while True:
+    user_guess = int(input("Pick a number between 1 and 1,000,000: "))
+
+    if user_guess < 0:
+        print("Now we'll never know what the answer is …")
+        exit()
+    elif user_guess < correct_number:
+        print("That number is too low. Try again!")
+        attempt += 1
+    elif user_guess > correct_number:
+        print("That number is too high. Try again!")
+        attempt += 1
+    elif user_guess == correct_number:
+        print("You are a winner! 🥳🥳")
+        print("It took you", attempt, "attempt(s) to get the correct answer.")
+        break
+    else:
+        print("That is not a number I recognize.")
