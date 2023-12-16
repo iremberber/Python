f0fad01a6db438d973a6b6510480141457e247b5
iremberber <iremberber0@gmail.com>
2023-12-16 17:56:20 +0300

    Day 25
    Return Command

--- replit-21-25DayOfPython.py
+++ replit-21-25DayOfPython.py
@@ -169,3 +169,74 @@ while game == "yes" or game == "Yes":
   game = input("Roll again?")
 
 rollDice(side)
+
+#day25
+
+#subroutine has parameter called `number`
+#`number` shows how many numbers we want the pin to have
+def pinPicker(number):
+  import random
+  pin = "" #this is the empty string
+  for i in range(number): #for loop shows defined amount of random numbers
+    pin += str(random.randint(0,9)) #we want a string of random numbers between 0-9
+  return pin
+
+myPin = pinPicker(4) #4 means we want 4 random numbers
+
+print(myPin)
+
+#CommonErrors
+"""
+def areaOfTriangle(base, height):
+  area = 0.5 * base * height
+  return area
+areaOfTriangle (5, 22)
+"""
+def areaOfTriangle(base, height):
+  area = 0.5 * base * height
+  return area
+myArea = areaOfTriangle (5, 22)
+print(myArea)
+
+"""
+def areaOfTriangle(base, height):
+  area = 0.5 * base * height
+  return area
+areaOfTriangle (5, 22)
+print(area)
+"""
+
+def areaOfTriangle(base, height):
+  area = 0.5 * base * height
+  return area
+area=areaOfTriangle (5, 22)
+print(area)
+
+#fixMyCode
+
+"""
+def area_square(side1 side2):
+  area = side1 * side2
+  return area_square
+area_square(4, 12)
+print(area)
+"""
+
+def area_square(side1, side2):
+  area = side1 * side2
+  return area
+area = area_square(4, 12)
+print(area)
+
+#Challenge
+
+import random
+
+def roll_dice(dice1,dice2):
+  healt = dice1*dice2
+  return healt
+healt = roll_dice(random.randint(1,6),random.randint(1,8))
+  
+
+name = input("What is your character's name?")
+print(name + "'s healt is " + str(healt)+ "ph")
