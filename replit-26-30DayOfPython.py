7c540d048e8f23bef387ad8224e55ba3351c67ca
iremberber <iremberber0@gmail.com>
2023-12-23 14:09:30 +0300

    Day 30
    f-strings

--- replit-26-30DayOfPython.py
+++ replit-26-30DayOfPython.py
@@ -197,3 +197,59 @@ newPrint("red", "new program")
 newPrint("reset", " I can just call red('and') ")
 newPrint("red", "it will print in red ")
 newPrint("blue", "or even blue")
+
+#day30
+
+name = "Katie"
+age = "28"
+pronouns = "she/her"
+
+print("This is {}, using {} pronouns, and is {} years old.".format(name, pronouns, age))
+
+name = "Katie"
+age = "28"
+pronouns = "she/her"
+print("This is {name}, using {pronouns} pronouns, and is {age} years old. Hello, {name}. How are you? Have you been having a great {age} years so far".format(name=name, pronouns=pronouns, age=age))
+
+name = "Katie"
+age = "28"
+pronouns = "she/her"
+response = f"This is {name}, using {pronouns} pronouns, and is {age} years old. Hello, {name}. How are you? Have you been having a great {age} years so far"
+print(response)
+
+for i in range(1, 31):
+  print(f"Day {i : ^2} of 30")
+
+#fixmycodepart
+
+"""
+food = pizza"
+location = "beach"
+color = "green"
+friend = "Quinn"
+"Alejandra ordered {food} to eat at the {location} with her friend, {friend}. She got to the {location} and looked for a {color} umbrella where {friend} said they would be waiting. By the time, Sally found {friend}, the {food} was cold and the {location} was lame." format(food=food, location=location, color=color,)
+print(response)
+"""
+
+food = "pizza"
+location = "beach"
+color = "green"
+friend = "Quinn"
+response = f"Alejandra ordered {food} to eat at the {location} with her friend, {friend}. She got to the {location} and looked for a {color} umbrella where {friend} said they would be waiting. By the time, Sally found {friend}, the {food} was cold and the {location} was lame."
+print(response)
+
+#challenge
+
+print("30 Days Down")
+
+i = 1
+while i < 31:
+  print(f"How was the Day {i}?")
+  answer = input()
+
+  response = f"Day {i} was {answer}."
+  i += 1
+  print(f"{response:^35}")
+
+else:
+  print("That's all you finish! Keep going!")
