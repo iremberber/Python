43765d498b9f7a564930e72651af073b5832ffcc
iremberber <iremberber0@gmail.com>
2023-12-29 23:29:49 +0300

    Update replit-36-40DayOfPython.py

--- replit-36-40DayOfPython.py
+++ replit-36-40DayOfPython.py
@@ -23,3 +23,63 @@ while True:
   printList()
 
 #common errors
+"""
+myList = []
+
+def printList():
+ print()
+ for i in myList:
+   print(i)
+ print()
+
+while True:
+ addItem = input("Item > ").capitalize().strip()
+ if addItem not in myList:
+   myList.append(addItem)
+ printList()
+"""
+
+myList = []
+
+def printList():
+ print()
+ for i in myList:
+   print(i)
+ print()
+
+while True:
+ addItem = input("Item > ").strip().capitalize()
+ if addItem not in myList:
+   myList.append(addItem)
+ printList()
+
+#challenge
+ 
+ listOfNames= []
+
+def printList():
+  print()
+  for name in listOfNames:
+    print(name)
+  print()
+
+while True:
+  firstName = input("First Name: ").strip().capitalize()
+  lastName = input("Last Name: ").strip().capitalize()
+  name = f"{firstName} {lastName}"
+  if name not in listOfNames:
+    listOfNames.append(name)
+  else:
+    print("ERROR: Duplicate name")
+  printList()
+
+#Day 37
+
+print("STAR WARS NAME GENERATOR")
+all = input("Enter your first name, last name, Mum's maiden name and the city you were born in").split()
+first = all[0].strip()
+last = all[1].strip()
+maiden = all[2].strip()
+city = all[3].strip()
+name = f"{first[:3].title()}{last[:3].lower()} {maiden[:2].title()}{city[-3:].lower()}"
+print(f"Your Star Wars name is {name}")
