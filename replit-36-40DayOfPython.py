0f012096d921f929e74ec0ceaeb4c979d33035fa
iremberber <iremberber0@gmail.com>
2024-01-07 00:17:40 +0300

    day 37

--- replit-36-40DayOfPython.py
+++ replit-36-40DayOfPython.py
@@ -75,3 +75,62 @@ while True:
 
 #Day 37
 
+myString = "Hello there my friend."
+print(myString[0])
+print(myString[6:11])
+print(myString[:11])
+print(myString[11:])
+
+#The Secret Third Argument
+
+myString = "Hello there my friend."
+print(myString[0:4:2])
+print(myString[::3])
+print(myString[::-1])
+
+#split
+
+myString = "Hello there my friend."
+print(myString.split())
+
+#Common Errors
+"""
+Why is it printing 'Hell' instead of 'Hello'?
+
+myString = "Hello there my friend."
+print(myString[0:4])
+"""
+
+myString = "Hello there my friend."
+print(myString[0:5])
+
+"""
+myString = "Hello there my friend."
+print(myString[0:4:0])
+"""
+
+myString = "Hello there my friend."
+print(myString[0:4:1])
+
+#fix my code
+
+"""
+# This code should output 'Hello there'
+myString = "Hello there my friend."
+print(myString[0:10:0])
+"""
+
+myString = "Hello there my friend."
+print(myString[0:11:1])
+
+#challenge
+
+print("Lets find your stage name!")
+
+name= input("What's your real name?")
+color = input("What's your favourite colour?")
+animal= input("what's your favourite animal?")
+
+stagename = name[0:3] + color[0:3] + animal[2:]
+
+print("Your stage name is ", stagename)
